e06a1d7414bc2864364cf5c1308f50fa3b33bbae
David Caldera <davidcaldera13@yahoo.com>
2022-02-23 10:47:46 -0600

    commit tree bd873be1c9154c2c598e51fc50b0909a1b73f407

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+function saturdayFun(fun) {
+    if (fun != null) {
+        return "This Saturday, I want to bathe my dog!";
+    } else {
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+
+function mondayWork(work) {
+    if (work != null) {
+        return "This Monday, I will work from home.";
+    } else {
+        return "This Monday, I will go to the office.";
+    }
+}
+
+function wrapAdjective(thing) {
+
+    if (thing === "*") {
+        function emphatic() {
+            return "You are *a hard worker*!";
+        }
+        return emphatic
+    } else if (thing === "||"){
+        function emphatic() {
+            return "You are ||a dedicated programmer||!"
+        }
+        return emphatic;
+    }
+}
